786e08cc00e4b24244241f5dcc06a5bb0beeacb4
Charles Morman <github@cmorman.com>
2024-11-11 16:03:40 -0700

    feat: basic rendering demo (The Hero) (plus some misc docstring updates)

--- app/components/renderable_component.py
+++ app/components/renderable_component.py
@@ -1,10 +1,19 @@
-"""Module: components/renderable_components.py"""
+"""
+Renderable Components Module
+
+This module defines the RenderableComponent abstract base class, which serves as a blueprint for all
+renderable or displayable objects within the application.
+
+Classes:
+    RenderableComponent: An abstract base class that requires implementing a render method to
+        display the component using a Renderer object.
+"""
 
 from abc import ABC, abstractmethod
 from app.renderers.renderer import Renderer
 
 
-class RenderableComponent:
+class RenderableComponent(ABC):
     """Abstract base class for all renderable/displayable objects for the application.
 
     Methods:
@@ -13,7 +22,8 @@ class RenderableComponent:
 
     @abstractmethod
     def render(self, renderer: "Renderer"):
-        """Calls the Renderer object to process and render this component.
+        """Calls the specific implementation to render this component when visited by a renderer
+        in a double-dispatch manner.
 
         Args:
             renderer: The renderer to use on this component.
