2306c8d611f118ae91df6c8814d1f746d0b95623
Charles Morman <github@cmorman.com>
2024-11-14 15:26:19 -0700

    feat: add abstract `render_pixelmatrix()` method to `Renderer` to allow for rendering a grouped pixel data.

--- app/renderers/renderer.py
+++ app/renderers/renderer.py
@@ -2,17 +2,16 @@
 Renderer Abstract Base Class Module
 
 This module defines the abstract base class `Renderer` for rendering pixel art. The `Renderer`
-class provides an interface for rendering individual pixels to various types of output or display.
+class provides an interface for rendering pixels to various types of outputs or displays.
 
 Classes:
     Renderer (ABC): Abstract base class for all Renderer objects.
-
-    render_pixel (abstract method): Renders a single pixel to some type of output or display.
 """
 
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from abc import ABC, abstractmethod
+from app.data.pixel_matrix import PixelMatrix
 
 # Break circular dependencies caused by type checking.
 if TYPE_CHECKING:
@@ -20,17 +19,29 @@ if TYPE_CHECKING:
 
 
 class Renderer(ABC):
-    """Abstract base class for all Renderer objects, which visit and generate an output for
+    """
+    Abstract base class for all Renderer objects, which visit and generate an output for
     RenderableComponent objects.
 
     Methods:
-        render_pixel: Renders a single pixel to some type of output or display.
+        render_pixel: Renders a single pixel to the renderer's display.
+        render_pixelmatrix: Renders a pixel matrix to to the renderer's display.
     """
 
     @abstractmethod
     def render_pixel(self, pixel: Pixel):
-        """Renders a single pixel to some type of output or display.
+        """
+        Renders a single pixel to the renderer's display.
 
         Args:
-            pixel (Pixel): The Pixel to render to the output.
+            pixel (Pixel): The Pixel object to render.
+        """
+
+    @abstractmethod
+    def render_pixelmatrix(self, pixel_matrix: PixelMatrix):
+        """
+        Renders a pixel matrix to to the renderer's display.
+
+        Args:
+            pixel_matrix (PixelMatrix): The PixelMatrix object to render.
         """
