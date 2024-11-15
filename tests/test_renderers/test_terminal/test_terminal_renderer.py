f0ab9196b1c8f784e5ae905025ad83af5ea76dcd
Charles Morman <github@cmorman.com>
2024-11-14 17:00:46 -0700

    refactor: clean up messy tests

--- tests/test_renderers/test_terminal/test_terminal_renderer.py
+++ tests/test_renderers/test_terminal/test_terminal_renderer.py
@@ -1,32 +1,30 @@
-import pytest
+"""
+Terminal Renderer Test Module
+"""
+
 from app.renderers.terminal.terminal_renderer import TerminalRenderer
 from app.components.pixel import Pixel
 
 
-@pytest.fixture
-def terminal_renderer():
-    """Generate a default TerminalRenderer fixture for tests to use"""
-    return TerminalRenderer()
-
-
-def test_initial_terminal_x_scale(terminal_renderer):
+def test_initial_terminal_x_scale():
     """Tests construction Renderer object with valid and invalid arguments"""
-    terminal_renderer = TerminalRenderer()
-    assert terminal_renderer.get_terminal_x_scale == 3
+    term_renderer = TerminalRenderer()
+    assert term_renderer.get_terminal_x_scale == 3
 
-    terminal_renderer = TerminalRenderer(terminal_x_scale=5)
-    assert terminal_renderer.get_terminal_x_scale == 5
+    term_renderer = TerminalRenderer(terminal_x_scale=5)
+    assert term_renderer.get_terminal_x_scale == 5
 
-    terminal_renderer = TerminalRenderer(terminal_x_scale=0)
-    assert terminal_renderer.get_terminal_x_scale == 1
+    term_renderer = TerminalRenderer(terminal_x_scale=0)
+    assert term_renderer.get_terminal_x_scale == 1
 
-    terminal_renderer = TerminalRenderer(terminal_x_scale=-5)
-    assert terminal_renderer.get_terminal_x_scale == 1
+    term_renderer = TerminalRenderer(terminal_x_scale=-5)
+    assert term_renderer.get_terminal_x_scale == 1
 
 
-def test_render_pixel(capsys, terminal_renderer):
+def test_render_pixel(capsys):
     """Test the rendered output for a pixel"""
     pixel = Pixel(x_pos=2, y_pos=3)
-    terminal_renderer.render_pixel(pixel)
+    term_renderer = TerminalRenderer()
+    term_renderer.render_pixel(pixel)
     captured = capsys.readouterr()
     assert captured.out == "\033[3;6H███\n"
